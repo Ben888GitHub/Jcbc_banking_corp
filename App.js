4680a7d0f0228760e554b7003103eda6be3d4354
winsontju <44856774+winsontju@users.noreply.github.com>
2019-11-21 15:41:46 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,33 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Mobile Banking Transfer')
+    const [outputText, setOutputText] = useState('Mobile Banking Transfer')
     return (
       <View style = { styles.container } >
-        <Text >{outputText}< /Text>
-        <View style = {styles.inputContainer}
-        <TextInput  />
-        </View>
-        <View style={padding: 40px}
-        <Button tittle="Click here to transfer" onPress{()=>setOutputText('Transfer Succeed!')}
-        </View>
-      </View>
-    );
-}
-
-const styles = StyleSh eet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      placeholder: 'Amount',
-      borderBottomColor:'black',
-      borderBottomWidth: 1
+        <Text> { outputText } < /Text>
+        <View style = { styles.inputContainer }>
+         <TextInput style = {  styles.inputContainer / >
+            <  View>
+            <  View style = { padding: 40 px }>
+             <  Button tittle = "Click here to transfer"
+            onPress {
+                () => setOutputText('Transfer Succeed!') }>
+                 </View>
+                  </View>
+                  </View>
+                 </View>
+        );
     }
-});
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        inputContainer: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            placeholder: 'Amount',
+            borderBottomColor: 'black',
+            borderBottomWidth: 1
+        }
+    });
