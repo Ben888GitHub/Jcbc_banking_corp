1ac4caba23be7c682b1ceb0478bb19ad05f9f4fb
winsontju <44856774+winsontju@users.noreply.github.com>
2019-11-21 09:40:41 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-    return ( <
-        View style = { styles.container } >
-        <
-        Text > Hi Winson < /Text>  <
-        /View>
+  const [outputText, setOutputText] = useState('Mobile Banking Transfer')
+    return (
+      <View style = { styles.container } >
+        <Text >{outputText}< /Text>
+        <View style = {styles.inputContainer}
+        <TextInput  />
+        </View>
+        <View style={padding: 40px}
+        <Button tittle="Click here to transfer" onPress{()=>setOutputText('Transfer Succeed!')}
+        </View>
+      </View>
     );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSh eet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      placeholder: 'Amount',
+      borderBottomColor:'black',
+      borderBottomWidth: 1
+    }
 });
