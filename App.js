f87efd96ec172b9f5784c9fa60a141fa5cc6df7f
KyleAng <kyleang7.08@gmail.com>
2019-11-22 10:19:07 +0800

    Fixed Syntax Erros

--- App.js
+++ App.js
@@ -4,19 +4,17 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
     const [outputText, setOutputText] = useState('Mobile Banking Transfer')
     return (
-      <View style = { styles.container } >
-        <Text> { outputText } < /Text>
-        <View style = { styles.inputContainer }>
-         <TextInput style = {  styles.inputContainer / >
-            <  View>
-            <  View style = { padding: 40 px }>
-             <  Button tittle = "Click here to transfer"
-            onPress {
-                () => setOutputText('Transfer Succeed!') }>
-                 </View>
-                  </View>
-                  </View>
-                 </View>
+        <View style = { styles.container } >
+            <Text> { outputText } </Text>
+            <View style = { styles.inputContainer }>
+            <TextInput style = {  styles.inputContainer  } />
+                <View style = {{ padding: 40 }}> 
+                    <Button title = "Click here to transfer"
+                    onPress={ () => alert('Transfer Succeed!') }>
+                    </Button>
+                </View>
+            </View>
+        </View>
         );
     }
 
@@ -30,7 +28,6 @@ export default function App() {
         inputContainer: {
             flexDirection: 'row',
             alignItems: 'center',
-            placeholder: 'Amount',
             borderBottomColor: 'black',
             borderBottomWidth: 1
         }
