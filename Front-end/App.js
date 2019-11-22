a9118d555c14bfe880f613a7b3ec1595d06f452d
Hung Nguyen <vanhung.nguyen2@my.jcu.edu.au>
2019-11-22 14:12:19 +0800

    Added the backend

--- Front-end/App.js
+++ Front-end/App.js
@@ -4,30 +4,32 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
     const [outputText, setOutputText] = useState('Mobile Banking Transfer')
     return (
-        <View style = { styles.container } >
-            <Text> { outputText } </Text>
-            <View style = { styles.inputContainer }>
-            <TextInput style = {  styles.inputContainer  } />
-                <View style = {{ padding: 40 }}> 
-                    <Button title = "Click here to transfer"
-                    onPress={ () => alert('Transfer Succeed!') }>
+        <View style={styles.container} >
+            <Text> {outputText} </Text>
+            <View style={styles.inputContainer}>
+                <TextInput style={[styles.inputContainer, {
+                    width: 200
+                }]} />
+                <View style={{ padding: 40 }}>
+                    <Button title="Click here to transfer"
+                        onPress={() => alert('Transfer Succeed!')}>
                     </Button>
                 </View>
             </View>
         </View>
-        );
-    }
+    );
+}
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        inputContainer: {
-            alignItems: 'center',
-            borderBottomColor: 'black',
-            borderBottomWidth: 1
-        }
-    });
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputContainer: {
+        alignItems: 'center',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1
+    }
+});
