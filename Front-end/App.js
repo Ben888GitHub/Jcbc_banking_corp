390f65a61e9e5903866b477eb8a255eb155af66e
winsontju <44856774+winsontju@users.noreply.github.com>
2019-11-22 14:59:01 +0800

    Add files via upload
    New Format, try look and fix if it does not run

--- Front-end/App.js
+++ Front-end/App.js
@@ -1,35 +1,64 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React from 'react';
+import { AppLoading } from 'expo';
+import { Container, Text, Header, Content, Button, Footer, FooterTab} from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
-    const [outputText, setOutputText] = useState('Mobile Banking Transfer')
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
     return (
-        <View style={styles.container} >
-            <Text> {outputText} </Text>
-            <View style={styles.inputContainer}>
-                <TextInput style={[styles.inputContainer, {
-                    width: 200
-                }]} />
-                <View style={{ padding: 40 }}>
-                    <Button title="Click here to transfer"
-                        onPress={() => alert('Transfer Succeed!')}>
-                    </Button>
-                </View>
-            </View>
-        </View>
+      <Success Button>
+        <Text>Transfer</Text>
+      </Button>
     );
+  }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputContainer: {
-        alignItems: 'center',
-        borderBottomColor: 'black',
-        borderBottomWidth: 1
-    }
-});
+export default class FooterTab extends React.Component {
+  render() {
+    return (
+      <Container>
+      <Header>
+      <Title> Mobile Banking Apps <Title />
+        <Header />
+        <Content />
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Icon name="Home" />
+            </Button>
+            <Button>
+              <Icon name="Account" />
+            </Button>
+            <Button active>
+              <Icon active name="Lifestyle" />
+            </Button>
+            <Button>
+              <Icon name="Services" />
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
