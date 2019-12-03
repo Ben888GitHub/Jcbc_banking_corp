3b6373cbe411c151f02efda40c611b19ca3e9cf4
Hung Nguyen <hungnguy@Hungs-MacBook-Air.local>
2019-12-03 16:42:05 +0800

    Add one more API

--- Back-end/app.js
+++ Back-end/app.js
@@ -3,9 +3,8 @@ const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 
-const CONNECTION_URL = "none";
-
-const DATABASE_NAME = "none";
+const CONNECTION_URL = "mongodb+srv://root:root@cluster0-djxyv.mongodb.net/test";
+const DATABASE_NAME = "users";
 
 var app = Express();
 const cors = require("cors");
@@ -19,6 +18,26 @@ var database, collection;
 
 app.get("/", (request, response) => {
     response.send('Hello');
+});
+
+app.post("/authenticate", async (request, response) => {
+    let pincode = request.body.pin;
+    let accname = request.body.accname;
+
+    let client = await MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true });
+    database = client.db(DATABASE_NAME);
+    collection = database.collection("userlist");
+
+    let requete = {
+        'accname': accname,
+        'pin': pincode
+    };
+    collection.find(requete).toArray((error, result) => {
+        if (error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
 });
 
 
