6e58e7f49dd7fbef223df66ade46668124a64451
Ben Ryan <benryan@yahoo.com>
2019-12-30 09:55:42 +0800

    emailOtpPage static

---
+++ Front-end/components/Otp.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import { StyleSheet, View, KeyboardAvoidingView, Keyboard } from "react-native";
+import OTPInputView from "@twotalltotems/react-native-otp-input";
+import {
+  Header,
+  Container,
+  Content,
+  Accordion,
+  Left,
+  Body,
+  Title,
+  Right,
+  Card,
+  CardItem,
+  Form,
+  Item,
+  Picker,
+  Icon,
+  Button,
+  Text,
+  Input,
+  Textarea
+} from "native-base";
+
+class Otp extends Component {
+  render() {
+    return (
+      <OTPInputView
+        style={{
+          width: "80%",
+          height: 200,
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+        pinCount={6}
+        // code={this.state.code} //You can supply this prop or not. The component will be used as a controlled / uncontrolled component respectively.
+        // onCodeChanged = {code => { this.setState({code})}}
+        autoFocusOnLoad
+        codeInputFieldStyle={styles.underlineStyleBase}
+        codeInputHighlightStyle={styles.underlineStyleHighLighted}
+        onCodeFilled={code => {
+          console.log(`Code is ${code}, you are good to go!`);
+          alert("You have successfully transfer");
+        }}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  borderStyleBase: {
+    width: 30,
+    height: 45
+  },
+
+  borderStyleHighLighted: {
+    borderColor: "#03DAC6"
+  },
+
+  underlineStyleBase: {
+    width: 40,
+    height: 30,
+    borderWidth: 0,
+    borderBottomWidth: 4,
+    borderColor: "lightgrey",
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 24,
+    marginTop: -95,
+    fontSize: 25
+  },
+
+  underlineStyleHighLighted: {
+    borderColor: "#03DAC6"
+  }
+});
+
+export default Otp;
---
+++ Front-end/screens/emailOtp.js
@@ -0,0 +1,173 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  Header,
+  Container,
+  Content,
+  Accordion,
+  Left,
+  Body,
+  Title,
+  Right,
+  Card,
+  CardItem,
+  Form,
+  Item,
+  Picker,
+  Icon,
+  Button,
+  Text,
+  Input,
+  Textarea
+} from "native-base";
+import axios from "axios";
+import Otp from "../components/Otp.js";
+import Countdown from "react-countdown";
+
+class emailOtp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageTitle: "Enter OTP  ",
+      resendCode: "Resend Code",
+      countdownTimer: " ",
+      timerStop: " ",
+      countdown: " " //TODO
+    };
+  }
+
+  componentDidMount() {}
+
+  _getOTP = () => {};
+
+  setTiming = () => {};
+
+  render() {
+    const countdown = ""; //TODO
+    const Completionist = () => <Text>You are good to go!</Text>;
+    const renderer = ({ seconds, completed }) => {
+      if (completed) {
+        // Render a completed state
+        return <Completionist />;
+      } else {
+        // Render a countdown
+        return <Text>{seconds}</Text>;
+      }
+    };
+    return (
+      // <View style={styles.container}>
+      <Container style={styles.container2}>
+        {/* <Text style={{fontSize: 60}}>{this.state.pageTitle}</Text> */}
+        <Header span style={styles.headerStyle}>
+          <Left>
+            <Button transparent>
+              <Icon
+                style={{ color: "white", marginLeft: 8 }}
+                name="arrow-back"
+              />
+            </Button>
+          </Left>
+          <Title style={styles.headerTitleStyle}>
+            {this.state.pageTitle}
+
+            <Icon style={{ color: "white" }} name="lock" />
+          </Title>
+          <Right />
+        </Header>
+        {/* */}
+        <Icon style={styles.userIconStyle} name="key" />
+        <Text
+          style={{
+            fontSize: 28,
+            marginHorizontal: 50,
+            marginVertical: -25,
+            marginLeft: 76
+          }}
+        >
+          Verification Code
+        </Text>
+        <Text></Text>
+        <Text
+          style={{
+            fontSize: 19,
+            marginHorizontal: 30,
+            marginVertical: 50
+          }}
+        >
+          6 Digits OTP has been send to your email
+        </Text>
+        {/* */}
+        {/* This section is for OTP Input */}
+        <Content>
+          <Otp />
+          <Text style={styles.timingStyle}>
+            Time Remaining:{" "}
+            <Countdown date={Date.now() + 20000} renderer={renderer} /> seconds
+            left
+          </Text>
+          <Text style={styles.resendCodeStyle}>
+            Didn't receive the OTP?
+            <Text
+              style={{
+                fontSize: 17,
+                fontWeight: "bold"
+              }}
+            >
+              {" "}
+              <Text></Text>
+              Resend Code
+            </Text>
+          </Text>
+        </Content>
+
+        {/* */}
+      </Container>
+      // </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  container2: {
+    backgroundColor: "#f7f7f7"
+  },
+  headerStyle: {
+    height: 100,
+    backgroundColor: "#c13b3e"
+  },
+  headerTitleStyle: {
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 25,
+    flexWrap: "wrap",
+    color: "#fff",
+    marginVertical: 20,
+    fontWeight: "normal"
+  },
+  userIconStyle: {
+    color: "grey",
+    fontSize: 120,
+    marginHorizontal: 160,
+    width: 120,
+    marginVertical: 40
+  },
+  timingStyle: {
+    marginVertical: -120,
+    marginHorizontal: 55,
+    marginBottom: 50
+  },
+  resendCodeStyle: {
+    marginVertical: 10,
+    marginHorizontal: 55,
+    fontSize: 17
+  }
+});
+
+export default emailOtp;
