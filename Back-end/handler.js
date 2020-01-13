e58682cd2429bcce3628498f4a5de543d9d9546c
nguyenvhung9420 <vanhung.nguyen2@my.jcu.edu.au>
2020-01-13 14:59:38 +0800

    Changed to serverless format - 2

--- Back-end/handler.js
+++ Back-end/handler.js
@@ -3,6 +3,43 @@ const awsServerlessExpress = require("aws-serverless-express");
 const app = require("./app");
 const server = awsServerlessExpress.createServer(app);
 
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const functions = require('./functions');
+const CONNECTION_URL = "mongodb+srv://root:root@cluster0-djxyv.mongodb.net/test";
+const DATABASE_NAME = "users";
+
 exports.cp3405 = (event, context) => {
   awsServerlessExpress.proxy(server, event, context);
 };
+
+
+// TESTING WITH THE SERVERLESS FORMAT:
+
+exports.authenticate_test = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  functions.connectToDatabase()
+    .then(collection => {
+      console.log(JSON.parse(event.body));
+      let requete = {
+        'accname': JSON.parse(event.body).accname,
+        'pin': JSON.parse(event.body).pin,
+      };
+      collection.find(requete).toArray((error, result) => {
+        if (error) {
+          return response.status(500).send(error);
+        }
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(result)
+        });
+      });
+    })
+    .catch(err => {
+      console.log('=> an error occurred: ', err);
+      callback(err);
+    });
+};
+
+// ENDING TESTING WITH THE SERVERLESS FORMAT.
