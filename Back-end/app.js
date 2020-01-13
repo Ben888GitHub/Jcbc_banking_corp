b8b242067b3e4cf4689d1bef95fc67a5ec48636b
nelsonnyan2001 <zwenyantoe@my.jcu.edu.au>
2020-01-13 11:18:36 +0800

    temporary fix

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,7 +1,9 @@
 const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
-const transfer = require('./functions');
+
+const functions = require('./functions');
+
 const CONNECTION_URL = "mongodb+srv://root:root@cluster0-djxyv.mongodb.net/test";
 const DATABASE_NAME = "users";
 
@@ -48,33 +50,124 @@ app.post("/authenticate", async (request, response) => {
 
 app.post("/transfer", async (request, response) => {
 
-    let sampleRequest = {
-        "sender_username": request.body.sender_username,
-        "source_acc_num": request.body.source_acc_num,
-        "transfer_amount": request.body.transfer_amount,
-        // "receiver_username": "1",
-        "dest_acc_num": request.body.dest_acc_num,
-    };
-
-    let repondre = transfer(sampleRequest);
-
-    response.status(200).send(repondre);
-});
-
-app.post("/transfer_byqr", async (request, response) => {
+    let client = await MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true });
+    database = client.db(DATABASE_NAME);
+    collection = database.collection("userlist");
 
     let sampleRequest = {
-        "sender_username": request.body.sender_username,
-        "source_acc_num": request.body.source_acc_num,
-        "transfer_amount": request.body.transfer_amount,
-        // "receiver_username": "1",
-        "dest_acc_num": request.body.qrcode,
+        "sender_username": "1",
+        "source_acc_num": "1",
+        "transfer_amount": 1,
+        "receiver_username": "1",
+        "dest_acc_num": "1",
     };
 
-    let repondre = transfer(sampleRequest);
 
-    response.status(200).send(repondre);
+    // Manipulating with the source:
+    let source_requete = {
+        $and: [
+            { "accname": request.body.sender_username },
+            { "accounts.accnumber": request.body.source_acc_num }
+        ]
+    };
+
+    let sourceAccNumber = request.body.source_acc_num;
+
+    let currentAccount = await collection.findOne(source_requete);
+
+    let currentAccList = currentAccount.accounts;
+    var currentBalance = currentAccList.find(function (eachAccount) {
+        if (eachAccount.accnumber === sourceAccNumber) {
+            return eachAccount;
+        };
+    });
+
+    let source_updateContent = {
+        $set: { "accounts.$.balance": currentBalance.balance - request.body.transfer_amount }
+    };
+    let source_result = await collection.updateOne(source_requete, source_updateContent);
+
+
+    // Manipulating with the destination:
+    let dest_requete = { "accounts.accnumber": request.body.dest_acc_num };
+
+    let destAccNumber = request.body.dest_acc_num;
+
+    currentAccount = await collection.findOne(dest_requete);
+    if (!currentAccount) {
+        console.log('Destination account is not exist.');
+        response.status(500).send({ errorMessage: 'Destination account is not exist.' });
+        return;
+    };
+
+    currentAccList = currentAccount.accounts;
+    currentBalance = currentAccList.find(function (eachAccount) {
+        if (eachAccount.accnumber === destAccNumber) {
+            return eachAccount;
+        };
+    });
+
+    if (currentBalance.balance < request.body.transfer_amount) {
+        response.status(500).send({ errorMessage: 'Source balance is not sufficient.' });
+        return;
+    };
+
+    let dest_updateContent = {
+        $set: { "accounts.$.balance": currentBalance.balance + request.body.transfer_amount }
+    };
+    let dest_result = await collection.updateOne(dest_requete, dest_updateContent);
+
+    let collectionTransactions = database.collection("transactions");
+    let timeStamp = JSON.stringify(Date.now());
+    let transactionID = request.body.sender_username + timeStamp;
+    let transactionRecord = await collectionTransactions.insertOne({
+        transactionid: transactionID,
+        sourceaccname: request.body.sender_username,
+        sourceaccnum: request.body.source_acc_num,
+        destinationaccname: "Not Defined",
+        destinationaccnum: request.body.dest_acc_num,
+        datestamp: timeStamp,
+        sourcecurrency: "SGD",
+        destinationcurrency: "SGD",
+        amount: request.body.transfer_amount,
+    })
+
+    response.status(200).send({
+        transactionRecord: transactionRecord,
+        substracting: source_result,
+        adding: dest_result
+    });
 });
+
+// app.post("/transfer", async (request, response) => {
+
+//     let sampleRequest = {
+//         "sender_username": request.body.sender_username,
+//         "source_acc_num": request.body.source_acc_num,
+//         "transfer_amount": request.body.transfer_amount,
+//         // "receiver_username": "1",
+//         "dest_acc_num": request.body.dest_acc_num,
+//     };
+
+//     let repondre = functions.transfer(sampleRequest);
+
+//     response.status(200).send(repondre);
+// });
+
+// app.post("/transfer_byqr", async (request, response) => {
+
+//     let sampleRequest = {
+//         "sender_username": request.body.sender_username,
+//         "source_acc_num": request.body.source_acc_num,
+//         "transfer_amount": request.body.transfer_amount,
+//         // "receiver_username": "1",
+//         "dest_acc_num": request.body.qrcode,
+//     };
+
+//     let repondre = functions.transfer(sampleRequest);
+
+//     response.status(200).send(repondre);
+// });
 
 app.post("/sendmail", async function (req, res) {
     let emailToSend = req.body.email;
